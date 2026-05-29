860688a850ee65484313b77260a66b2e26a96c5d
MisterB,,24 <lakatbor@gmail.com>
2026-05-29 10:53:33 +0200

    Buttons funktionieren jetzt im Start screen

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,42 @@
 import GUI.Brett;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main extends JFrame {
-
+    JFrame frame;
     public static void main(String[] args) {
-        JFrame frame = new Main();
-        frame.setSize(1000, 1000);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
+        new Main();
     }
     public Main(){
-        Brett board = new Brett();
-        add(board);
+        frame = this;
+        setSize(1000, 1000);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        WilkommenScreen start = new WilkommenScreen();
+        add(start);
+        setVisible(true);
     }
-    //Test
+    public class WilkommenScreen extends JPanel{
+        private JButton Einzelspieler;
+        private JButton Multiplayer;
+        private JButton Einstellungen;
+
+        public WilkommenScreen(){
+            setLayout(new GridLayout(1,1));
+            Einzelspieler = new JButton("Einzelspieler");
+            Multiplayer = new JButton("Multiplayer");
+            Einstellungen = new JButton("Einstellungen");
+            Einzelspieler.addActionListener(e -> {
+                Container c = frame.getContentPane();
+                c.removeAll();
+                Brett board = new Brett();
+                c.add(board, BorderLayout.CENTER);
+                c.revalidate();
+                c.repaint();
+            });
+            add(Einzelspieler);
+            add(Multiplayer);
+            add(Einstellungen);
+        }
+    }
 }
