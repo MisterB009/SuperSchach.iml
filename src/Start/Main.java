74d6048db5db6c9ddba72155ebe82cc524b4b61d
MisterB024 <lakatbor@gmail.com>
2026-06-01 18:03:27 +0200

    Startscreen graphisch angepasst

---
+++ src/Start/Main.java
@@ -0,0 +1,163 @@
+package Start;//import GUI.MouseHover;
+import GUI.Brett;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Main extends JFrame {
+    JFrame frame;
+    public static void main(String[] args) {
+        new Main();
+    }
+    public Main(){
+        frame = this;
+        //setSize(1000, 1000);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        WilkommenScreen start = new WilkommenScreen();
+        add(start);
+        setVisible(true);
+    }
+    public class WilkommenScreen extends JPanel{
+        private JLabel Start;
+        private JButton Einzelspieler;
+        private JButton Multiplayer;
+        private JButton Einstellungen;
+        private Image hintergrund;
+        Color dG = new Color(110, 110,110);
+        Color dGHover = new Color(140, 140, 140);
+
+
+        @Override
+        public void paintComponent(Graphics g){
+            try {
+                hintergrund= ImageIO.read(new File("img/Start Hintergrund.png"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            g.drawImage(hintergrund,0,0,this.getWidth(),this.getHeight(),null);
+            super.paintComponent(g);
+        }
+
+        public WilkommenScreen(){
+            setOpaque(false);
+            MouseStart mouseStart = new MouseStart(this);
+            this.setLayout(new GridBagLayout());
+
+
+            Start = new JLabel("MegaSchach");
+            Font groeseSchrift = new Font("Arial", Font.BOLD, 30);
+            Start.setFont(groeseSchrift);
+            Start.setForeground(Color.WHITE);
+            GridBagConstraints c = new GridBagConstraints();
+            c.insets = new Insets(0,0,50,0);
+            c.gridx = 0;
+            c.gridy = 0;
+            add(Start,c);
+
+
+            Einzelspieler = new JButton("Einzelspieler"){
+                @Override
+                protected void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                    if (getModel().isRollover()) {
+                        g2.setColor(dGHover);
+                    } else {
+                        g2.setColor(dG);
+                    }
+                    g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+                    g2.dispose();
+                    super.paintComponent(g);
+                }
+            };
+            Einzelspieler.setFocusPainted(false);
+            Einzelspieler.setBorderPainted(false);
+            Einzelspieler.setContentAreaFilled(false);
+            Einzelspieler.setForeground(Color.WHITE);
+            Einzelspieler.addMouseListener(mouseStart);
+            Einzelspieler.addActionListener(e -> {
+                getContentPane();
+                removeAll();
+                this.setLayout(new BorderLayout());
+                Brett board = new Brett();
+                add(board);
+                revalidate();
+                repaint();
+            });
+            c = new GridBagConstraints();
+            c.gridx = 0;
+            c.insets = new Insets(0,0,20,0);
+            c.gridy = 1;
+            c.gridwidth = 10;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            add(Einzelspieler, c);
+
+
+            Multiplayer = new JButton("Multiplayer"){
+                @Override
+                protected void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                    if (getModel().isRollover()) {
+                        g2.setColor(dGHover);
+                    } else {
+                        g2.setColor(dG);
+                    }
+                    g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+                    g2.dispose();
+                    super.paintComponent(g);
+                }
+            };
+            Multiplayer.setFocusPainted(false);
+            Multiplayer.setBorderPainted(false);
+            Multiplayer.setContentAreaFilled(false);
+            Multiplayer.setForeground(Color.WHITE);
+            Multiplayer.addMouseListener(mouseStart);
+            Multiplayer.addActionListener(e -> {
+
+            });
+            c = new GridBagConstraints();
+            c.gridx = 0;
+            c.insets = new Insets(0,0,20,0);
+            c.gridy = 2;
+            c.gridwidth = 10;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            add(Multiplayer, c);
+
+
+            Einstellungen = new JButton("Einstellungen"){
+                @Override
+                protected void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                    if (getModel().isRollover()) {
+                        g2.setColor(dGHover);
+                    } else {
+                        g2.setColor(dG);
+                    }
+                    g2.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
+                    g2.dispose();
+                    super.paintComponent(g);
+                }
+            };
+            Einstellungen.setFocusPainted(false);
+            Einstellungen.setBorderPainted(false);
+            Einstellungen.setContentAreaFilled(false);
+            Einstellungen.setForeground(Color.WHITE);
+            Einstellungen.addMouseListener(mouseStart);
+            Einstellungen.addActionListener(e -> {
+
+            });
+            c = new GridBagConstraints();
+            c.gridx = 0;
+            c.gridy = 3;
+            c. gridwidth = 10;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            add(Einstellungen, c);
+        }
+    }
+}
---
+++ src/Start/MouseStart.java
@@ -0,0 +1,44 @@
+package Start;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseStart implements MouseListener {
+    Main.WilkommenScreen func;
+    Color dG = new Color(110, 110,110);
+    Color dGHover = new Color(140, 140, 140);
+    public MouseStart(Main.WilkommenScreen func){
+        super();
+        this.func = func;
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        JButton hilfBtn = (JButton)e.getSource();
+        hilfBtn.setBackground(dGHover);
+        hilfBtn.setForeground(Color.BLUE);
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        JButton hilfBtn = (JButton)e.getSource();
+        hilfBtn.setBackground(dG);
+        hilfBtn.setForeground(Color.WHITE);
+    }
+}
